809506752787d5f97b54bb756ecb1229bc6f37e0
Matt Bell <mappum@gmail.com>
2012-07-10 20:43:04 -0700

    Fixed models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
+var mongoose = require('mongoose');
+
 module.exports = {
 	'User': mongoose.model('User', require(__dirname + '/user.js')),
 	'Outfit': mongoose.model('Outfit', require(__dirname + '/outfit.js')),
