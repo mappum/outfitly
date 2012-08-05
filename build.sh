43f02acd8d7b455aa9e0fc78f5f9e0bb666b6d01
Matt Bell <mappum@gmail.com>
2012-08-04 17:27:14 -0700

    Added build script for production files

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Concatenating CSS"
+cat ./public/css/bootstrap.css > ./public/css/all.css
+cat ./public/css/bootstrap-responsive.css >> ./public/css/all.css
+cat ./public/css/outfitly.css >> ./public/css/all.css
+
+echo "Concatenating JS"
+cat ./public/js/bootstrap.js > ./public/js/all.js
+cat ./public/js/underscore.js >> ./public/js/all.js
+cat ./public/js/backbone.js >> ./public/js/all.js
+cat ./public/js/torso.js >> ./public/js/all.js
+cat ./public/js/moment.js >> ./public/js/all.js
+cat ./public/js/outfitly.js >> ./public/js/all.js
+
+echo "Minifying CSS"
+java -jar ./tools/yuicompressor*.jar -o ./public/css/all.min.css ./public/css/all.css
+
+echo "Minifying JS"
+java -jar ./tools/yuicompressor*.jar -o ./public/js/all.min.js ./public/js/all.js
+
+echo "Build complete."
