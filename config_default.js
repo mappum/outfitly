195a25ebd7cb01d25df83c45a834397dcc6beeb3
Matt Bell <mappum@gmail.com>
2012-08-14 14:04:39 -0700

    Updated config_default

--- config_default.js
+++ config_default.js
@@ -8,36 +8,60 @@ var YEAR = DAY * 365;
 var config = {};
 
 // general site metadata
+var domain = 'localhost'; // your domain
 config.meta = {
-	host: 'HOST',
-	port: 80,
-	dataHost: 'DATA_HOST',
+	domain: domain,
+	webHost: domain, // the host we want to get static web files from
+	apiHost: domain, // the host we want to request data from
+
+	port: 80, // the port this app is listening on
+
 	title: 'Outfitly',
 	tagline: ''
 };
 
+// feed settings
+config.feed = {
+	// whether or not to show users the global feed (show all posts, not only following)
+	global: true
+};
+
 // database settings
 config.mongo = {
-	user: 'DB_USER',
-	password: 'DB_PASSWORD',
-	host: 'DB_HOST',
-	db: 'DB_NAME'
+	user: '',
+	password: '',
+	host: 'localhost',
+	db: 'outfitly'
 };
 var m = config.mongo;
-m.uri = 'mongodb://' + m.user + ':' + m.password + '@' + m.host + '/' + m.db;
+config.mongo.uri = 'mongodb://' + m.user + ':' + m.password + '@' + m.host + '/' + m.db;
 
-// cookie settings
-config.cookie = {
-	secret: 'SECRET',
-	key: 'session',
-	maxAge: MONTH
+// redis settings
+config.redis = {
+	host: '',
+	port: 1337,
+	db: 'outfitly',
+	pass: ''
+};
+
+// session settings
+config.session = {
+	cookie: {
+		secret: '',
+		key: 'session',
+		expires: false
+	},
+	ignore: [
+		'/users/exists',
+		'/products'
+	]
 };
 
 // static server settings
 config.static = {
 	enabled: true,
 	path: __dirname + '/public',
-	age: WEEK
+	age: HOUR
 };
 
 // ssl settings
@@ -50,26 +74,41 @@ config.ssl = {
 config.auth = {
 	requireVerification: false
 };
-
-//external service settings
+a
+// external service settings
 config.externals = {
 	'facebook': {
-		appId: 'APP_ID',
-		appSecret: 'APP_SECRET',
+		appId: '',
+		appSecret: '',
 		scope: 'email',
-		callback: config.url + '/auth/callback/facebook'
+		callback: 'http://' + config.meta.apiHost + '/auth/callback/facebook'
+	},
+
+	'twitter': {
+		consumerKey: '',
+		consumerSecret: ''
 	}
 };
 
 // mail settings
-config.smtp = {
-	host: "SMTP_HOST",
-    secureConnection: true,
-    port: 465,
-	auth: {
-		user: 'SMTP_USER',
-		pass: 'SMTP_PASSWORD'
+config.mail = {
+	enabled: false, // TODO: set up mails
+	smtp: {
+		host: '',
+		secureConnection: true,
+		port: 465,
+		auth: {
+			user: '',
+			pass: ''
+		}
 	}
+};
+
+// amazon product advertising API settings
+config.apa = {
+	id: '',
+	secret: '',
+	associateId: ''
 };
 
 module.exports = config;
